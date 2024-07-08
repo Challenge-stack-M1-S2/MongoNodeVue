3b7c80ec45d05eec4c71daf5beb5709ced5f9ca3
orian <30657132+oriancm@users.noreply.github.com>
2024-07-08 17:51:06 +0200

    Update conf.js

--- app/config/conf.js
+++ app/config/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGODB_USER: 'oriancm',
-    MONGODB_PWD: 'TsekB4BCXkoyp1HP',
+    MONGODB_PWD: '',
     MONGODB_CLUSTER: 'cluster0.mpbkmsa.mongodb.net',
     MONGODB_DATABASE: 'flashart',
     MONGOOSE_DEBUG: 'false'
