473afe9baff2a34e354c077288c3d78458bc5d88
orian <30657132+oriancm@users.noreply.github.com>
2024-07-08 15:27:38 +0200

    Init project

---
+++ app/config/conf.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONGODB_USER: 'oriancm',
+    MONGODB_PWD: 'TsekB4BCXkoyp1HP',
+    MONGODB_CLUSTER: 'cluster0.mpbkmsa.mongodb.net',
+    MONGODB_DATABASE: 'eval-node',
+    MONGOOSE_DEBUG: 'false'
+  };
+  
