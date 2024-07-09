df6c40058acc0afda66d45d0b0f676207326c93f
Côme Bonal <come.bonal@gmail.com>
2024-07-09 13:03:05 +0200

    ajout de la bannière et diverse modif page accueil

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,6 +3,7 @@ import Card from "@/components/CardComponent.vue";
 </script>
 
 <template>
+  <img src="../../img/banner.jpg" alt="" />
   <container class="w-4/5 grid mx-auto">
     <Card></Card>
   </container>
