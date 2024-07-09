6a89ce2302833c142a52a786ab83da0c013ee0fb
Côme Bonal <come.bonal@gmail.com>
2024-07-09 14:20:56 +0200

    aze

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,7 +3,11 @@ import Card from "@/components/CardComponent.vue";
 </script>
 
 <template>
-  <img class="w-full h-80" src="" alt="" />
+  <img
+    class="w-full h-80"
+    src="/MongoNodeVue/client/src/assets/banner.png"
+    alt=""
+  />
   <container class="w-4/5 grid mx-auto">
     <Card></Card>
   </container>
