104f4d5fba6b96f0ba76de06011bb577734d930e
orian <30657132+oriancm@users.noreply.github.com>
2024-07-09 13:01:58 +0200

    Delete app directory

--- app/models/style.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (mongoose) => {
-const Style = mongoose.model(
-    'Style',
-    new mongoose.Schema({
-    style_name: { type: String, required: true }
-    })
-);
-return Style;
-}
