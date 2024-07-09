0d2018776e676e23a627d57a204f639babedd154
Côme Bonal <come.bonal@gmail.com>
2024-07-09 15:32:28 +0200

    changement sur la main page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <script setup>
 import Card from "@/components/CardComponent.vue";
-import banner from "../assets/banner.png";
+import banner from "../assets/banner2.png";
 </script>
 
 <template>
   <img class="w-full" :src="banner" alt="" />
-  <div class="flex bg-black h-80 w-1/2">
-    <div></div>
-    <div></div>
-    <div></div>
+  <div class="flex h-80 w-1/2 absolute top-28 right-0 justify-between">
+    <div
+      class="border-t-4 border-b-4 border-l-4 border-white h-full w-5/12"
+    ></div>
+    <div class="border-t-4 border-b-4 border-white h-full w-1/5"></div>
+    <div class="border-t-4 border-b-4 border-white h-full w-1/5"></div>
   </div>
-  <container class="w-4/5 grid mx-auto">
+  <container class="w-4/5 grid mx-auto pt-4">
     <Card></Card>
   </container>
 </template>
