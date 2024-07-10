2568618b1e7abe0926a4a311b0d90fbb5bd72df7
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2024-07-10 10:34:18 +0200

    login

---
+++ client/src/views/LoginForm.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="login-container">
+      <form @submit.prevent="handleLogin">
+        <h2>Login</h2>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" required />
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      handleLogin() {
+        // Logique de gestion de la connexion
+        console.log('Email:', this.email);
+        console.log('Password:', this.password);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  
+  form {
+    width: 300px;
+    padding: 2rem;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-color: #fff; /* Fond blanc pour le formulaire */
+  }
+  h2 {
+    text-align: center;
+    margin-bottom: 1rem;
+    color: black;
+  }
+  
+  .form-group {
+    margin-bottom: 1rem;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    color: #333; /* Couleur du texte des labels */
+  }
+  
+  input {
+    width: 100%;
+    padding: 0.5rem;
+    box-sizing: border-box;
+    border: 1px solid #ccc; /* Bordure pour les inputs */
+    border-radius: 4px;
+    color: #333; /* Couleur du texte des inputs */
+    background-color: #f9f9f9; /* Fond des inputs */
+  }
+  
+  button {
+    width: 100%;
+    padding: 0.5rem;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }
+  </style>
+  
