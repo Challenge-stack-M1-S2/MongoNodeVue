ef375a55540efb3d20d23ee7c6dc51a6c5531881
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2024-07-12 10:48:15 +0200

    fix sidemenu height v2

--- client/src/components/SideMenu.vue
+++ client/src/components/SideMenu.vue
@@ -5,6 +5,7 @@
       'background-image':
         'url(' + require('@/assets/sidemenu-banner2.png') + ')',
     }"
+    style="height: 100vw"
   >
   <nav class="w-full h-screen flex flex-col">
     <!-- Logo Section -->
