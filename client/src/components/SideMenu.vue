d189ef96c8ff4e2b11e7d7000ea9043ecadaf5a1
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2024-07-12 10:52:20 +0200

    fix language

--- client/src/components/SideMenu.vue
+++ client/src/components/SideMenu.vue
@@ -97,7 +97,7 @@
               src="https://cdn-icons-png.flaticon.com/512/12765/12765311.png"
               class="fas fa-star mr-2"
             />
-            Login
+            Connection
           </router-link>
         </li>
         <li v-else class="mb-4">
