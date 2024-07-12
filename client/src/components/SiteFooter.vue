c3025210ad60e27c8c054f514b9eea75f35eafa2
antoine-falgiglio <antoine.falgiglio@bedrockstreaming.com>
2024-07-12 10:42:51 +0200

    fix details

--- client/src/components/SiteFooter.vue
+++ client/src/components/SiteFooter.vue
@@ -1,10 +1,10 @@
 <template>
   <footer class="bg-gray-800 text-white p-4 text-center">
-    <p>&copy; 2024 Inked Legends. All Rights Reserved.</p>
+    <p>&copy; 2024 Inked Legends. Tous droits réservés.</p>
     <nav>
       <ul class="flex justify-center space-x-4">
-        <li><router-link to="/" class="hover:text-gray-400">Home</router-link></li>
-        <li><router-link to="/about" class="hover:text-gray-400">About</router-link></li>
+        <li><router-link to="/" class="hover:text-gray-400">Accueil</router-link></li>
+        <li><router-link to="/about" class="hover:text-gray-400">À Propos</router-link></li>
         <li><router-link to="/contact" class="hover:text-gray-400">Contact</router-link></li>
       </ul>
     </nav>
